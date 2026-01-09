1c7ae2737929117157e1f5dc6a6be29b1762c4a8
Driton Gashi <dritongashi1995@gmail.com>
2026-01-09 16:08:06 +0100

    removed unused files

--- api/index.ts
+++
@@ -1,7 +0,0 @@
-// Vercel serverless function for Express API
-// This file is in /api/index.ts to be recognized by Vercel as a serverless function
-// All requests to /api/* will be handled by this Express app
-// Note: Server must be built first (npm run build:server) before this will work
-import app from '../server/dist/index';
-
-export default app;
